701e4969345868608f453e2f1bf1e04646a46b67
Uladzislau Kastsevich <uladzislau_kastsevich@epam.com>
2018-04-02 18:10:35 +0300

    RZHAKA

--- FrontEnd/app/es-add-product-modal/es-add-product-modal.component.js
+++ FrontEnd/app/es-add-product-modal/es-add-product-modal.component.js
@@ -8,8 +8,41 @@ export var esAddProductModalComponent = {
     }
 };
 
-function AddProductModalController(productService, errorMessageService, $state) {
+function AddProductModalController(productService, productTypeService, errorMessageService, $state) {
     var $ctrl = this;
+
+    $ctrl.$onInit = function () {
+        $ctrl.product = {
+            types: []
+        };
+        productTypeService.getAllProductTypes().then(function (types) {
+            $ctrl.types = types;
+        });
+    };
+
+    $ctrl.selectType = function ($item, $model, $label, $event) {
+        $ctrl.typesInput = "";
+
+        var typeIndex = $ctrl.product.types.findIndex(function (type, index, types) {
+            if (type.id == $item.id) {
+                return true;
+            }
+        });
+
+        if (typeIndex == -1) {
+            $ctrl.product.types.push($item);
+        }
+    };
+
+    $ctrl.removeType = function (typeToRemove) {
+        var typeIndex = $ctrl.product.types.findIndex(function (type, index, types) {
+            if (type.id == typeToRemove.id) {
+                return true;
+            }
+        });
+
+        $ctrl.product.types.splice(typeIndex, 1);
+    };
 
     $ctrl.save = function () {
         var promise = productService.addProduct($ctrl.product);

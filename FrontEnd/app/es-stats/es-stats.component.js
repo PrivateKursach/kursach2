a282a141ff837384afbedc74dd2a8a5606562705
Uladzislau Kastsevich <uladzislau_kastsevich@epam.com>
2018-04-03 04:10:12 +0300

    RZHAKA

--- FrontEnd/app/es-stats/es-stats.component.js
+++ FrontEnd/app/es-stats/es-stats.component.js
@@ -6,15 +6,34 @@ var esStatsComponent = {
 function StatsComponentController(statsService, errorMessageService) {
     var $ctrl = this;
 
+    $ctrl.salesByTypeChartData = {
+        chart: {
+            caption: "Статистика продаж по категориям",
+            subcaption: "",
+            startingangle: "120",
+            showlabels: "0",
+            showlegend: "1",
+            enablemultislicing: "0",
+            slicingdistance: "15",
+            showpercentvalues: "1",
+            showpercentintooltip: "0",
+            plottooltext: "Категория : $label Продажи : $datavalue",
+            theme: "fint"
+        },
+        data: []
+    };
+
     $ctrl.$onInit = function () {
         statsService.getSalesByTypesStats().then(function (stats) {
+            console.log(stats);
             var data = [];
             stats.forEach(function (item, i, array) {
                 data.push({
                     label: item.productTypeName,
-                    datavalue: item.totalSales
+                    value: item.totalSales
                 })
             });
+            console.log(data);
             $ctrl.salesByTypeChartData = {
                 chart: {
                     caption: "Статистика продаж по категориям",
